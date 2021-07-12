7341f05ea0991257f8f950adaf16b04ae7990a6f
ADESH ANAND <adeshanand.official@gmail.com>
2021-07-13 00:04:04 +0530

    code optimized

--- file_separator.py
+++ file_separator.py
@@ -1,33 +1,27 @@
 import os, shutil
 
-audio = ('.mp3','.m4a','.wav','.flac')
-video = ('.mp4','.mkv','.MKV','.flv','.mpeg')
-document = ('.docx','.pdf','.txt')
-image = ('.png','.jpeg')
-executable = ('.exe','.com')
 
-path = input('enter directory path : ')
-
-os.chdir(path)
-
-data = os.walk(path)
+file_extensions = {
+    'audio' : ('.mp3','.m4a','.wav','.flac'),
+    'video' : ('.mp4','.mkv','.MKV','.flv','.mpeg'),
+    'document' : ('.docx','.pdf','.txt'),
+    'image' : ('.png','.jpeg'),
+    'executable' : ('.exe','.com')
+}
 
 def copy_file(folder_name,file_path,file):
     if not os.path.exists(folder_name):
         os.mkdir(folder_name)
     shutil.copy(os.path.join(file_path,file),folder_name + '/' + file)
 
+scan_folder_path = input('enter directory path : ')
+os.chdir(scan_folder_path)
+data = os.walk(scan_folder_path)
+
 for file_path, _, files in data:
     if files:
         for file in files:
             _, extension = os.path.splitext(file)
-            if extension in audio:
-                copy_file('audio',file_path,file)
-            if extension in video:
-                copy_file('video',file_path,file)
-            if extension in document:
-                copy_file('document',file_path,file)
-            if extension in image:
-                copy_file('image',file_path,file)
-            if extension in executable:
-                copy_file('executable',file_path,file)
+            for file_extension_format, file_extension_values in file_extensions.items():
+                if extension in file_extension_values:
+                    copy_file(file_extension_format,file_path,file)
